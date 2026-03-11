7aa484b44e9be82a50cb7eab127fe7b5f691cd9b
arun <routpratik09@gmail.com>
2026-03-11 15:37:26 +0530

    set up frontend and backend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold text-center mt-10 bg-red-900 p-5 rounded-lg shadow-md'>
+      welcome to flight booking platform
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+
+connectDB();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Flight Booking API Running"
+  });
+});
+
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found"
+  });
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
